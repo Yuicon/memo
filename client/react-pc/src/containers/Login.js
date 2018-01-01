b9383a98407c7d2ac6ccf0aeddfc67396c9440e3
Yuicon <910722178@qq.com>
2018-01-01 22:50:05 +0800

    feat:登陆基本页面

---
+++ client/react-pc/src/containers/Login.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+export default class Login extends Component {
+
+    render() {
+        return(
+            <div className="flex">
+                <main className="flex login">
+                    <h3>登陆</h3>
+                    <label htmlFor="email">邮箱</label>
+                    <input type="email" name="email" id="email"/>
+                    <label htmlFor="secretKey">密匙</label>
+                    <input type="text" name="secretKey" id="secretKey"/>
+                    <label htmlFor="masterPassword">主密码</label>
+                    <input type="password" name="masterPassword" id="masterPassword"/>
+                </main>
+            </div>
+        );
+    }
+
+
+}

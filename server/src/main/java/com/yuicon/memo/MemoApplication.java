a426a4a3ddab35d4e691bb9e381da5a9d4b7579b
Yuicon <910722178@qq.com>
2018-01-02 22:39:46 +0800

    feat:配置全局 cors

--- server/src/main/java/com/yuicon/memo/MemoApplication.java
+++ server/src/main/java/com/yuicon/memo/MemoApplication.java
@@ -3,6 +3,9 @@ package com.yuicon.memo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Yuicon
+ */
 @SpringBootApplication
 public class MemoApplication {
 

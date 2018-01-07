25391a489a63e9dd8a434890976be2eae021b09c
Yuicon <910722178@qq.com>
2018-01-07 16:24:21 +0800

    chore: react-route prop-types

--- client/react-pc/src/App.js
+++ client/react-pc/src/App.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
+import {observer, Provider} from "mobx-react";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from "./containers/Login";
 
+@observer
 class App extends Component {
     render() {
+
+        const rootStore = this.props.rootStore;
+
         return (
-            <main className="App">
-                <Login/>
-            </main>
+            <Provider
+                rootStore={rootStore}
+            >
+                <Router>
+                    <main className="App">
+                        <Route exact path="/" component={Login}/>
+                    </main>
+                </Router>
+            </Provider>
         );
     }
 }
+
+App.propTypes = {
+    rootStore: PropTypes.object,
+};
 
 export default App;

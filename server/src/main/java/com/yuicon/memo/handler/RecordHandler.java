2fd1bf17b7e45e43d36bdb811cf23eb182c9200e
Yuicon <910722178@qq.com>
2018-01-10 00:25:51 +0800

    feat:保存用户备忘录接口

--- server/src/main/java/com/yuicon/memo/handler/RecordHandler.java
+++ server/src/main/java/com/yuicon/memo/handler/RecordHandler.java
@@ -1,12 +1,17 @@
 package com.yuicon.memo.handler;
 
 import com.yuicon.memo.domain.Record;
+import com.yuicon.memo.domain.User;
 import com.yuicon.memo.repository.RecordRepository;
+import com.yuicon.memo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
@@ -17,15 +22,24 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 public class RecordHandler {
 
     private final RecordRepository recordRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public RecordHandler(RecordRepository recordRepository) {
+    public RecordHandler(RecordRepository recordRepository, UserRepository userRepository) {
         this.recordRepository = recordRepository;
+        this.userRepository = userRepository;
     }
 
     public Mono<ServerResponse> save(ServerRequest request) {
-        Mono<Record> passwordRecordMono = request.bodyToMono(Record.class);
-        return ServerResponse.ok().contentType(APPLICATION_JSON).body(this.recordRepository.insert(passwordRecordMono), Record.class);
+        Flux<User> userFlux = request.bodyToMono(Record.class)
+                .flatMap(this.recordRepository::insert)
+                .flux()
+                .flatMap(record -> this.userRepository.findById(request.queryParam("uid").orElse(""))
+                        .flatMap(user -> {
+                            user.setRecords(Collections.singletonList(record));
+                            return this.userRepository.save(user);
+                        }));
+        return ServerResponse.ok().contentType(APPLICATION_JSON).body(userFlux, User.class);
     }
 
     public Mono<ServerResponse> records(ServerRequest request) {
@@ -35,7 +49,6 @@ public class RecordHandler {
     public Mono<ServerResponse> delete(ServerRequest request) {
         return ServerResponse.ok().contentType(APPLICATION_JSON).body(this.recordRepository.deleteById(request.pathVariable("id")), Void.class);
     }
-
 
 
 }
